240979e813ecea039f972e7d50ad62bb465a990f
Keith <keithtmccartney@hotmail.com>
2018-11-14 21:17:08 +0000

    Applying changes to the 'model' representing an object from the database.

--- src/musicstore/album/model.go
+++ src/musicstore/album/model.go
@@ -1,1 +1,19 @@
 // album/model.go
+
+package album
+
+import (
+    "database/sql"
+    _ "github.com/mattn/go-sqlite3"
+)
+
+//Album represents a music album
+type Album struct {
+    ID int `json:"_id"`
+    Title sql.NullString `json:"title"`
+    Artist sql.NullString `json:"artist"`
+    Year datetime `json:"year"`
+}
+
+//Albums is an array of Album
+type Albums []Album
